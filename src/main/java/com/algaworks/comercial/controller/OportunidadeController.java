c54714ae16bd02c4913e48b093cd7806cc13cac0
Lucas P <lucas_wuthang@hotmail.com>
2019-06-08 19:03:31 -0300

    Finalizando API

--- src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
+++ src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
@@ -4,16 +4,21 @@ package com.algaworks.comercial.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.algaworks.comercial.model.Oportunidade;
 import com.algaworks.comercial.repository.OportunidadeRepository;
@@ -21,7 +26,7 @@ import com.algaworks.comercial.repository.OportunidadeRepository;
 @RestController
 @RequestMapping("oportunidades")
 public class OportunidadeController {
-	
+
 	//injecao dependencia spring
 	@Autowired
 	private OportunidadeRepository oportunidades;
@@ -30,22 +35,55 @@ public class OportunidadeController {
 	public List<Oportunidade> listar() {
 		return this.oportunidades.findAll();
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<Oportunidade> buscar(@PathVariable Long id) {
 		Optional<Oportunidade> op = this.oportunidades.findById(id);
-		
-		if(op.equals("")) {
+
+		if(!op.isPresent()) {
 			return ResponseEntity.notFound().build();
 		}
-		
+
 		return ResponseEntity.ok(op.get());
 	}
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Oportunidade adicionar(@RequestBody Oportunidade op) {
+	public Oportunidade adicionar(@Valid @RequestBody Oportunidade op) {
+		Optional<Oportunidade> oportunidadeExistente = 
+				this.oportunidades.findByDescricaoAndNomeProspecto(op.getDescricaoOportunidade(), op.getNomeProspecto());
+
+		if(oportunidadeExistente.isPresent()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "já existe uma oportunidade para este prospecto com a mesma descricação");
+		}
+
 		return this.oportunidades.save(op);
+	}
+
+	@PutMapping("/alterar")
+	public Oportunidade alterar(Oportunidade op) {
+
+		Optional<Oportunidade> oportunidadeExistente = this.oportunidades.findById(op.getId());
+
+		if(oportunidadeExistente.isPresent()) {
+			this.oportunidades.save(op);
+		}
+		
+		return op;
+
+	}
+	
+	@DeleteMapping("excluir/{id}")
+	public ResponseEntity<Oportunidade> deletar(@PathVariable Long id) {
+		Optional<Oportunidade> op = this.oportunidades.findById(id);
+		
+		if(op.isPresent()) {
+			this.oportunidades.deleteById(id);
+			return ResponseEntity.ok(op.get());
+		}
+		
+		return ResponseEntity.notFound().build();
+		
 	}
 
 }
